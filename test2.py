f9cf7c8eed144f54886cada6e85485ea999682de
gavin-edward <gavin.edward0521@outlook.com>
2024-10-15 11:41:49 +0800

    evans commit modified test2.py

--- test2.py
+++ test2.py
@@ -1,8 +1,11 @@
 import os
 import sys
 import tensorflow as tf
+import pandas as pd
 import matplotlib.pyplot as plt
+
 
 print(sys.path)
 print(sys.path.append("/home/xxx/project/vip"))
 print(tf.__version__)
+print(pd.__version__)
